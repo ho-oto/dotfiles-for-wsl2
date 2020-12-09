7a9b5a874bcbfe43e86f291745581a054d87ede7
Hayate Nakano <nakanodesu@gmail.com>
2020-12-10 00:11:20 +0900

    move files

---
+++ .config/fish/config.fish
@@ -0,0 +1,3 @@
+set fish_greeting
+set -x FZF_LEGACY_KEYBINDINGS 0
+starship init fish | source
---
+++ .julia/config/startup.jl
@@ -0,0 +1,7 @@
+using Pkg
+if any(pkg.name == "OhMyREPL" for (id, pkg) in Pkg.dependencies())
+    @eval using OhMyREPL
+else
+    @warn("OhMyREPL not installed")
+end
+# ENV["PLOTS_DEFAULT_BACKEND"] = "unicodeplots"
