8a2fc985f08fb7c29660d504aa9445ac04f257a3
Hayate Nakano <nakanodesu@gmail.com>
2020-12-15 02:16:35 +0900

    update abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,18 @@ set -x FZF_LEGACY_KEYBINDINGS 0
 set PATH /usr/local/cuda/bin $PATH
 set LD_LOAD_PATH /usr/local/cuda/lib64 $LD_LOAD_PATH
 
+alias ls="exa --icons --group-directories-first --git"
+alias ncdu="ncdu --color dark -rr"
+
+abbr --add em emacs
+
+abbr --add ga git add
+abbr --add gau git add --update
+abbr --add gaa git add --all
+abbr --add gc git commit
+abbr --add gca git commit --amend
+abbr --add gco git checkout
+abbr --add gst git status
+abbr --add gdf git diff
+
 starship init fish | source
