f45b6ca3709da70bd7a24a322e1658f6550877c8
Hayate Nakano <nakanodesu@gmail.com>
2021-01-01 03:16:18 +0900

    use OhMyREPL only in REPL

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,12 +1,11 @@
-using Pkg
-if any(pkg.name == "OhMyREPL" for (id, pkg) in Pkg.dependencies())
-    @eval using OhMyREPL
-else
-    @warn("OhMyREPL not installed")
-end
-
 ENV["JULIA_CUDA_USE_BINARYBUILDER"] = "false"
 
+using Pkg
 atreplinit() do repl
     ENV["PLOTS_DEFAULT_BACKEND"] = "unicodeplots"
+    if any(pkg.name == "OhMyREPL" for (id, pkg) in Pkg.dependencies())
+        @eval using OhMyREPL
+    else
+        @warn("OhMyREPL not installed")
+    end
 end
