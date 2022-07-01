459920bf7ba98c46060dcebca366fced38e8d764
Hayate Nakano <nakanodesu@gmail.com>
2022-07-01 15:08:37 +0900

    up

--- windows/start_wsl_sshd.bat
+++ windows/start_wsl_sshd.bat
@@ -1,4 +1,4 @@
-wsl -d Ubuntu-20.04 -u root --exec sudo service ssh restart
-for /F %%i in ('wsl -d Ubuntu-20.04 --exec hostname -I') do set ip=%%i
+wsl -u root --exec sudo service ssh restart
+for /F %%i in ('wsl --exec hostname -I') do set ip=%%i
 netsh interface portproxy delete v4tov4 listenport=22
 netsh interface portproxy add    v4tov4 listenport=22 connectaddress=%ip%
